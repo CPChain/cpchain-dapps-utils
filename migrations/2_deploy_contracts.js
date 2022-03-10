52fa7b6f415417a0b6f9bddcd89c1cdb0bc5c7ba
zgljl2012 <2693491512@qq.com>
2022-03-10 11:24:02 +0800

    add BaseERC20 and SafeMath

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,6 @@
 var Example = artifacts.require("./Example.sol");
 var OwnerExample = artifacts.require("./OwnerExample.sol");
 module.exports = function (deployer) {
-        deployer.deploy(Example); //"参数在第二个变量携带"
-        deployer.deploy(OwnerExample); //"参数在第二个变量携带"
+        deployer.deploy(Example);
+        deployer.deploy(OwnerExample);
 };
---
+++ safeMath.js
@@ -0,0 +1,7 @@
+const Example = artifacts.require("SafeMath");
+const truffleAssert = require('truffle-assertions');
+contract("Test SafeMath", (accounts) => {
+  it("add", async () => {
+
+  })
+})
